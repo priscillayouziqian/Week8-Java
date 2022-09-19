ebe6a44936fa944e53f4068f138543cbbe42f011
Zhaoyi Yan <priscillayouziqian@gmail.com>
2022-09-19 18:00:19 -0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    int dateOfBirth; //variable for storing date of birth of user
+    //single line comment
+
+    /*
+    This class is main class and starting point of this project.
+    */
+
+//    try shortcut: Ctrl_shilt
+
+    /*
+     *this is comment again, short cut Ctrl+shilt+/
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
